f51c612814ca5fea2506b48145ff5794ff96cf80
Chris Wilson <chris@chris-wilson.co.uk>
2020-01-24 13:01:07 +0000

    drm: Avoid drm_global_mutex for simple inc/dec of dev->open_count
    Since drm_global_mutex is a true global mutex across devices, we don't
    want to acquire it unless absolutely necessary. For maintaining the
    device local open_count, we can use atomic operations on the counter
    itself, except when making the transition to/from 0. Here, we tackle the
    easy portion of delaying acquiring the drm_global_mutex for the final
    release by using atomic_dec_and_mutex_lock(), leaving the global
    serialisation across the device opens.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Thomas Hellström (VMware) <thomas_os@shipmail.org>
    Reviewed-by: Thomas Hellström <thellstrom@vmware.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200124130107.125404-1-chris@chris-wilson.co.uk

--- drmkpi/include/asm/atomic.h
+++ drmkpi/include/asm/atomic.h
@@ -55,6 +55,7 @@ typedef struct {
 #define	atomic_inc_and_test(v)		(atomic_add_return(1, (v)) == 0)
 #define	atomic_dec_return(v)		atomic_sub_return(1, (v))
 #define	atomic_inc_not_zero(v)		atomic_add_unless((v), 1, 0)
+#define	atomic_fetch_inc(v)		(atomic_inc_return(v) - 1)
 
 static inline int
 atomic_add_return(int i, atomic_t *v)
